80fadcead4e30b5401f64051ba64314a7d3ed45b
ajaylogaom1 <ajaylogaom06@gmail.com>
2024-11-11 16:43:57 +0530

    first commit

---
+++ weather app/src/App.jsx
@@ -0,0 +1,17 @@
+
+import Weatherapp from './Weatherapp';
+import './Infobox.css'
+
+import Button from '@mui/material/Button';
+function App() {
+  let handleClick =() => {
+    console.log("button was clicked");
+  };
+ return(
+  <>
+  <Weatherapp />
+  </>
+ );
+}
+
+export default App;
---
+++ weather app/src/Weatherapp.jsx
@@ -0,0 +1,24 @@
+import Searchbox from "./Searchbox";
+import Infobox from"./Infobox";
+import { useState } from 'react';
+export default function Weatherapp(){
+    const [weatherInfo , setWeatherInfo]=useState({
+        City: "Delhi",
+        feelsLike:24.84,
+        temp:25.05,
+        tempMin:25.05,
+        tempMax:25.05,
+        humidity:47,
+        weather: "haze",
+    });
+    let updateInfo = (newinfo)=>{
+        setWeatherInfo(newinfo);
+    }
+    return(
+        <div style={{textAlign:"center"}}>
+            <h2>Weatherapp </h2>
+            <Searchbox updateInfo={updateInfo}/>
+            <Infobox info={weatherInfo}/>
+        </div>
+    );
+}
